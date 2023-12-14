b34019e7dbf8d2a3f4ac07b5f10fb5e308a7ab57
corneliustanui <kiplimocornelius@gmail.com>
2023-12-14 20:37:54 +0300

    Placed custom funs in files where they are used

--- app.R
+++ app.R
@@ -1,8 +1,7 @@
 
 # load scripts
-source("Funs/custom_table_funs.R")
-source("ui.R")
-source("server.R")
+source("./ui.R")
+source("./server.R")
 
 # Preview the app
 shinyApp(ui = ui, server = server)
