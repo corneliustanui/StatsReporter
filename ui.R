aae5f2cdf90157bf58fa8e02a92e1ca249922040
corneliustanui <kiplimocornelius@gmail.com>
2023-12-14 15:46:41 +0300

    Updated UI and server elements

--- ui.R
+++ ui.R
@@ -1,19 +1,19 @@
 # packages
 library(shiny)
 library(shinydashboard)
+library(shinyWidgets)
 library(tidyverse)
 library(magrittr)
-library(table1)
-
-# load data
-# STIData <- read.csv("C:/Users/CORNELIUS/OneDrive/Folders/Data/STIData_Clean.csv")
+library(DT)
+library(renv)
 
 # Define UI
 ui <- navbarPage(
   title = strong("Stats Reporter"),
   
-  tabPanel(title = NULL, icon = icon("chart-simple", lib = "font-awesome"),
-           
+  tabPanel(title = NULL, 
+           icon = icon("chart-simple"),
+
            column(width = 4,
                   
                   h3("Import data file"),
@@ -34,49 +34,93 @@ ui <- navbarPage(
                                  width = NULL,
                                  size = NULL),
                   
-                  varSelectInput(inputId = "secondary_var",
-                                 label = "Select secondary variable",
-                                 data = NULL,
-                                 selected = NULL,
-                                 multiple = FALSE,
-                                 selectize = TRUE,
-                                 width = NULL,
-                                 size = NULL),
+                  awesomeCheckbox("show_secondary_var", label = "Add a grouping variable?", value = FALSE),
                   
-                  varSelectInput(inputId = "tertiary_var", 
-                                 label = "Select tertiary variable",
-                                 data = NULL,
-                                 selected = NULL,
-                                 multiple = FALSE,
-                                 selectize = TRUE,
-                                 width = NULL,
-                                 size = NULL),
-                  
-                  h3("Generate report"),
-                  actionButton("generate_report", "Run"),
-                  
-                  actionButton("clear_report", "Clear report"),
-                  
-                  h3("Download report"),
-                  
-                  checkboxGroupInput(inputId = "report_type",
-                                     label = "Report file type",
-                                     choices = c(".docx", ".pdf", ".html"),
+                  conditionalPanel(
+                    
+                    condition = "input.show_secondary_var == true",
+                    
+                    varSelectInput(inputId = "secondary_var",
+                                   label = "Select secondary variable",
+                                   data = NULL,
+                                   selected = NULL,
+                                   multiple = FALSE,
+                                   selectize = TRUE,
+                                   width = NULL,
+                                   size = NULL),
+                    
+                    awesomeCheckbox("show_tertiary_var", label = "Add a second variable?", value = FALSE),
+                    
+                    conditionalPanel(
+                      condition = "input.show_tertiary_var == true",
+                      
+                      varSelectInput(inputId = "tertiary_var", 
+                                     label = "Select tertiary variable",
+                                     data = NULL,
                                      selected = NULL,
-                                     inline = TRUE,
-                                     width = NULL),
+                                     multiple = FALSE,
+                                     selectize = TRUE,
+                                     width = NULL,
+                                     size = NULL)
+                    )
+                  ),
+
+                  h3("Generate report"),
+                  actionButton(inputId = "generate_report", 
+                               label = "Run",
+                               icon = icon ("play"),
+                               style = "background-color:#317EBD;
+                                        color:#FFFFF7;
+                                        border-color:#BEBEBE;
+                                        border-style:none;
+                                        border-width:0px;
+                                        border-radius:7%;
+                                        font-size:13px;
+                                        width:30#"),
                   
-                  downloadButton("download_demo_data", label = "Download")
+                  actionBttn(inputId = "clear_report",
+                             label = "Clear report",
+                             icon = icon("eraser"),
+                             style = "stretch",
+                             color = "success",
+                             size = "sm",
+                             block = FALSE,
+                             no_outline = FALSE)
                   
-              ),
+           ),
            
-           column(width = 4,
+           column(width = 8,
                   tabsetPanel(
-                    tabPanel("Data", DTOutput("raw_table")),
+                    tabPanel("Data", 
+                             
+                             icon = icon ("clipboard-list"),
+                             
+                             div(DT::dataTableOutput("raw_table"), 
+                                 style = "font-size: 70%; width: 70%")),
+
+                    tabPanel("Tables", 
+                             
+                             icon = icon ("lightbulb"),
+                             
+                             h3("Download report"),
+                             
+                             checkboxGroupInput(inputId = "report_type",
+                                                label = "Report file type",
+                                                choices = c(".docx", ".pdf", ".html"),
+                                                selected = NULL,
+                                                inline = TRUE,
+                                                width = NULL),
+                             
+                             downloadButton("download_dt_table", label = "Download"),
+
+                             div(tableOutput("dt_table"), 
+                                 style = "font-size: 70%; width: 70%")
+                             
+                             ),
                     
-                    tabPanel("Tables", "some tables"),
-                    
-                    tabPanel("Graphs", "List of graphs")
+                    tabPanel("Graphs", 
+                             icon = icon ("magnifying-glass-chart"), 
+                             "List of graphs")
                     )
              )
       )
